ab1c581214130201c00b6af037fff3b1440fe6aa
CRISPYTYPER <jack3409@naver.com>
2023-03-07 11:48:19 +0900

    add www.gamefeed.net to ALLOWED_HOSTS

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.200.213.85', 'gamefeed.net']
+ALLOWED_HOSTS = ['43.200.213.85', 'gamefeed.net', 'www.gamefeed.net']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
